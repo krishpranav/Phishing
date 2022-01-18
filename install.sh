cf93e2716feb52351faabccb268d7525b908cec6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2022-01-18 08:43:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@ echo "UPDATING"
 apt update
 clear
 echo "INSTALLING PHP"
-sudo apt install php
+apt install php
 clear
 echo "INSTALLING OpenSSH"
-sudo apt install openssh
+apt install openssh
 clear
 echo "INSTALLING UNZIP"
-sudo apt install unzip
+apt install unzip
 clear
 echo "INSTALLING WGET"
-sudo apt install wget
+apt install wget
 clear
 echo "INSTALLING CURL"
-sudo apt install curl
+apt install curl
 echo "SUCCESSFULL INSTALLED ALL DEPDENDENCIES"
 echo "NOW RUN bash phishing.sh"
